f155f9c2daef8b48bf978a27f30ffcaf840b1c8f
Gaffen <matt@gaffen.co.uk>
2016-12-22 22:55:55 +0000

    migrate nearly complete

---
+++ web/wp-content/mu-plugins/index.php
@@ -0,0 +1,4 @@
+<?php
+require_once WPMU_PLUGIN_DIR.'/advanced-custom-fields-pro/acf.php';
+require_once WPMU_PLUGIN_DIR.'/force-regenerate-thumbnails/force-regenerate-thumbnails.php';
+require_once WPMU_PLUGIN_DIR.'/timber-library/timber.php';

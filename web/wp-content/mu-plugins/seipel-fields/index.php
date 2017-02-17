6ec1dcd281363f94eca0456ec88d0e862d4001c1
Gaffen <matt@gaffen.co.uk>
2017-02-17 18:14:47 +0000

    first commit

---
+++ web/wp-content/mu-plugins/seipel-fields/index.php
@@ -0,0 +1,31 @@
+<?php
+// require_once 'inc/acf.php';
+
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+
+function my_acf_json_save_point( $path ) {
+
+    // update path
+    $path = plugin_dir_path( __FILE__ ) . 'local-json';
+
+    // return
+    return $path;
+
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_load_point( $paths ) {
+
+    // remove original path (optional)
+    unset($paths[0]);
+
+
+    // append path
+    $paths[] = plugin_dir_path( __FILE__ ) . 'local-json';
+
+
+    // return
+    return $paths;
+
+}

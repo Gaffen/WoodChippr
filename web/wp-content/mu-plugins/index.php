8b0239aa69412b762c4a330d9735d448ea501cd8
Gaffen <matt@gaffen.co.uk>
2017-05-30 15:13:15 +0100

    start undoing accidental seipel changes

--- web/wp-content/mu-plugins/index.php
+++ web/wp-content/mu-plugins/index.php
@@ -2,4 +2,4 @@
 require_once WPMU_PLUGIN_DIR.'/advanced-custom-fields-pro/acf.php';
 require_once WPMU_PLUGIN_DIR.'/force-regenerate-thumbnails/force-regenerate-thumbnails.php';
 require_once WPMU_PLUGIN_DIR.'/timber-library/timber.php';
-require_once WPMU_PLUGIN_DIR.'/seipel-fields/index.php';
+require_once WPMU_PLUGIN_DIR.'/wordpress-seo/wp-seo.php';

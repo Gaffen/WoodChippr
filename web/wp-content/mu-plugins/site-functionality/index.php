f61187182c7ddb3310374ad8063595088be6a1c8
Gaffen <matt@gaffen.co.uk>
2017-07-10 14:39:03 +0100

    add functionality plugin

---
+++ web/wp-content/mu-plugins/site-functionality/index.php
@@ -0,0 +1,51 @@
+<?php
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+
+function my_acf_json_save_point( $path ) {
+
+    // update path
+    $path = plugin_dir_path( __FILE__ ) . 'local-json';
+
+    // return
+    return $path;
+
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_load_point( $paths ) {
+
+    // remove original path (optional)
+    unset($paths[0]);
+
+
+    // append path
+    $paths[] = plugin_dir_path( __FILE__ ) . 'local-json';
+
+
+    // return
+    return $paths;
+
+}
+
+// function my_acf_init() {
+//
+// 	acf_update_setting('google_api_key', get_field('gmaps_key', 'option'));
+// }
+//
+// add_action('acf/init', 'my_acf_init');
+
+// function cc_mime_types($mimes) {
+//   $mimes['svg'] = 'image/svg+xml';
+//   return $mimes;
+// }
+// add_filter('upload_mimes', 'cc_mime_types');
+//
+// function custom_admin_head() {
+//   $css = '';
+//
+//   $css = 'td.media-icon img[src$=".svg"] { width: 100% !important; height: auto !important; }';
+//
+//   echo '<style type="text/css">'.$css.'</style>';
+// }
+// add_action('admin_head', 'custom_admin_head');

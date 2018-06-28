5a10ab78748333d54e49384d56cd3b82e1a538aa
Matthew Gaffen <matt@gaffen.co.uk>
2018-06-28 12:18:59 +0100

    Upgrade outdated npm packages with security vulnerabilities
    Migrated to gulp 4 and upgreaded as many outdated packages as possible.

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,60 @@
 require('dotenv').config();
 // Include gulp
-var requireDir    = require('require-dir'),
-    gulp          = require("gulp"),
-
-// Include Our Plugins
-    notify        = require("gulp-notify"),
-    livereload    = require("gulp-livereload");
+var requireDir = require('require-dir'),
+	gulp = require('gulp'),
+	// Include Our Plugins
+	notify = require('gulp-notify'),
+	livereload = require('gulp-livereload');
 
 requireDir('./gulp', { recurse: true });
 
-// Watch Files For Changes
-gulp.task("startwatch", ["lint", "sass", "scripts", "svgstore", "watch"]);
-
-gulp.task("watch", function() {
-  "use strict";
-  livereload.listen();
-  gulp.watch(
-    process.env.DEV_FILES_DIR+'/'+process.env.JS_DIR+"/**/**/*.js",
-    ["lint", "scripts"]
-  );
-  gulp.watch(
-    process.env.DEV_FILES_DIR+'/'+process.env.SCSS_DIR+"/**/**/*.scss",
-    ["sass"]
-  );
-  gulp.watch(
-    process.env.DEV_FILES_DIR+'/'+process.env.SVG_DIR+"/**/**/*.svg",
-    ["svgstore"]
-  );
-  gulp.watch("web/wp-content/**/**/**/*.twig").on("change", function(file){
-    livereload.reload(file);
-    notify("Templates Refreshed");
-  });
+gulp.task('watch', function() {
+	'use strict';
+	livereload.listen();
+	gulp.watch(
+		process.env.DEV_FILES_DIR + '/' + process.env.JS_DIR + '/**/**/*.js',
+		gulp.parallel('lint', 'scripts')
+	);
+	gulp.watch(
+		process.env.DEV_FILES_DIR + '/' + process.env.SCSS_DIR + '/**/**/*.scss',
+		gulp.parallel('sass')
+	);
+	gulp.watch(
+		process.env.DEV_FILES_DIR + '/' + process.env.SVG_DIR + '/**/**/*.svg',
+		gulp.parallel('svgstore')
+	);
+	var liveReloadWatcher = gulp.watch(
+		'web/wp-content/**/**/**/*.twig',
+		function() {
+			return true;
+		}
+	);
+	liveReloadWatcher.on('change', function(file) {
+		livereload.reload(file);
+		notify('Templates Refreshed');
+	});
 });
 
-gulp.task("build", ["lint", "sass", "scripts", "svgstore"]);
+// Watch Files For Changes
+gulp.task(
+	'startwatch',
+	gulp.series('lint', 'sass', 'scripts', 'svgstore', 'watch', function() {
+		done();
+	})
+);
 
-gulp.task("default", ["lint", "sass", "scripts", "svgstore"]);
+gulp.task(
+	'build',
+	gulp.series('lint', 'sass', 'scripts', 'svgstore'),
+	function() {
+		done();
+	}
+);
+
+gulp.task(
+	'default',
+	gulp.series('lint', 'sass', 'scripts', 'svgstore'),
+	function() {
+		done();
+	}
+);
